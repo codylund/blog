489e852bc2f988f359c460842e439afb3bee661e
c <c@cs-MacBook-Pro.local>
2018-10-14 22:30:01 -0400

    Fetch sample.blog from repo

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,23 +1,44 @@
+#[macro_use]
+extern crate serde_derive;
+
+extern crate base64;
 extern crate futures;
 extern crate hyper;
 extern crate hyper_tls;
+extern crate serde;
+extern crate serde_json;
+extern crate tokio_core;
 
 fn main() {
     use blog_repository::Connection;
+    use tokio_core::reactor::Core;
 
-    println!("Hello, world!");
+    // We will run tasks using tokio
+    let mut core = Core::new().unwrap();
+
+    // Prepare the connection to the blog post repository
     let blog_repo_connection = Connection::new();
-    let test_page = blog_repo_connection.get_blog_post("/testpage");
+
+    // Request the sample blog post and get the reponse future
+    let sample_blog_post = "sample.blog";
+    println!("Getting {0} from repo github.com/codylund/blog", sample_blog_post);
+    let test_page_request = blog_repo_connection.get_blog_post(sample_blog_post);
+
+    // Run the request task and get the content
+    let content = core.run(test_page_request);
+    println!("Done: {:?}", content.unwrap());
 }
 
 mod blog_repository {
 
+    use base64;
+    use hyper::Error;
+    use hyper::rt::Future;
     use network;
-    use std::time::Duration;
+    use serde_json;    
+    use std::str;
 
-    const TEST_URL: &str = "https://api.github.com/repos/codylund/onestep/contents/app/src/main/java/com/codylund/onestep/viewmodels/StepViewModel.kt";
-    const URL: &str = "https://api.github.com/repos/codylund/blog/contents/blog";
-    const METADATA: &str  = "/metadata.json";
+    const BLOG_POST_ROOT: &str = "https://api.github.com/repos/codylund/blog/contents/blog/";
 
     pub struct Connection {
         network_connection: network::Connection,
@@ -30,32 +51,42 @@ mod blog_repository {
             Connection { network_connection }
         }
 
-        // fn getMetadataForBlogPosts(&self) -> Vec<BlogPostMetadata> {
-        //     self.networkConnection.get(URL + METADATA);
-        // }
+        pub fn get_blog_post(&self, filename: &str) -> impl Future<Item = String, Error = Error> {
+            let full_path = [BLOG_POST_ROOT, filename].concat();
+            self.network_connection.get(full_path)
+                .map(|content| {
+                    // Parse the blog content from the JSON
+                    let encoded_blog_post: BlogPost = serde_json::from_str(&content).unwrap();
 
-        pub fn get_blog_post(&self, path: &str) {
-            let fullPath = [TEST_URL].concat();
-            self.network_connection.get(fullPath);
+                    // Remove the trailing newline (TODO: properly detect before stripping)
+                    let mut encoded_content = encoded_blog_post.content;
+                    encoded_content.pop();
+
+                    // Decode base64 content
+                    let string_vec = base64::decode(&encoded_content).unwrap();
+
+                    // Send the final content on its merry way!
+                    str::from_utf8(&string_vec)
+                        .unwrap()
+                        .to_owned()
+                })
         }
 
     }
 
-    struct BlogPostMetadata {
-        title: String,
-        description: String,
-        path: String,
-        date_created: Duration,
-        date_modified: Duration,
+    #[derive(Serialize, Deserialize, Debug)]
+    struct BlogPost {
+        content: String,
     }
-
 }
 
 mod network {
 
     use hyper;
     use hyper_tls;
-    use hyper::rt::{self, Future};
+    use hyper::rt::Future;
+    use futures::Stream;
+    use std::str;
 
     pub struct Connection {
         client: hyper::Client<hyper_tls::HttpsConnector<hyper::client::HttpConnector>>,
@@ -64,27 +95,35 @@ mod network {
     impl Connection {
 
         pub fn new() -> Connection {
+            // Add TLS because GitHub API uses HTTPS
             let https = hyper_tls::HttpsConnector::new(4).expect("TLS initialization failed");
             let client = hyper::Client::builder()
                     .build::<_, hyper::Body>(https);
             Connection { client }
         }
 
-        pub fn get(&self, url: String) {
+        pub fn get(&self, url: String) -> impl Future<Item = String, Error = hyper::Error> {
+            // Add an arbitrary User-Agent header so GitHub is happy
             let request = hyper::Request::get(url)
                     .header("User-Agent", "codylund")
                     .body(hyper::Body::empty())
                     .unwrap();
-            rt::run(self.client
-                .request(request)
-                .map(|res| {
-                    println!("Response: {}", res.status())
-                })
-                .map_err(|err| {
-                    println!("Error: {}", err);
-                })
-            );
-        }
 
+            // Send the request and return the future response
+            self.client
+                .request(request)
+                .and_then(|res| {
+                    // Concatenate the response chunks
+                    res
+                        .into_body()
+                        .concat2()
+                })
+                .map(move |chunk| {
+                    // Convert the raw response bytes to a string
+                    str::from_utf8(&chunk)
+                        .unwrap()
+                        .to_owned()
+                })
+        }
     }
 }
